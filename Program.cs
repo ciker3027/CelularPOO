2a3f5b89b4a408ca5a65e8ba587b876ace881934
Erick <erick.kkk@hotmail.com>
2022-11-06 13:42:06 -0300

    conclusão

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using CELULARPOO.Models;
+
+Smartphone nokia = new Nokia("876345120", "N454",111111111,64);
+Smartphone iphone = new Iphone("895673214","7pro",44444444,64);
+
+nokia.Ligar();
+nokia.InstalarAplicativo("Cobrinha");
+nokia.MostrarNumero();
+nokia.Numero = "11111111";
+nokia.MostrarNumero();
+
+iphone.InstalarAplicativo("Instagram");
+iphone.ReceberLigacao();
--- README.md
+++
@@ -1,1 +0,0 @@
-# CelularPOO
